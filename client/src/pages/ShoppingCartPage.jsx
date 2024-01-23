e4c3977696ff2190d0bf7ed4dd3aa6ab4e9884ed
nicolasrrdev <nicolasreinalesst@gmail.com>
2024-01-23 12:54:24 -0500

    Realizar Compra

--- client/src/pages/ShoppingCartPage.jsx
+++ client/src/pages/ShoppingCartPage.jsx
@@ -1,113 +1,154 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const ShoppingCartPage = () => {
-    const [carrito, setCarrito] = useState([]);
+  const [carrito, setCarrito] = useState([]);
 
-    useEffect(() => {
-        const carritoData = JSON.parse(localStorage.getItem("carrito")) || [];
-        setCarrito(carritoData);
-        // Add the carrito state variable to the dependency array.
-    }, []);
+  useEffect(() => {
+    const carritoData = JSON.parse(localStorage.getItem("carrito")) || [];
+    setCarrito(carritoData);
+  }, []);
 
-    const eliminarDelCarrito = (id) => {
-        const index = carrito.findIndex((item) => item._id === id);
-
-        if (index !== -1) {
-            carrito.splice(index, 1);
-
-            actualizarCarritoEnLocalStorage(carrito);
-            obtenerCarrito();
-        }
-    };
-
-    const actualizarCarritoEnLocalStorage = (carritoData) => {
-        localStorage.setItem("carrito", JSON.stringify(carritoData));
-    };
-
-    const obtenerCarrito = () => {
-        const carritoData = JSON.parse(localStorage.getItem("carrito")) || [];
-        setCarrito(carritoData);
-    };
-
-    const sumarTotal = (productos) => {
-        // Inicializa el total
-        let total = 0;
-
-        // Suma los valores totales de los productos
-        for (const producto of productos) {
-            total += producto.valortotal;
-        }
-
-        return total;
-    };
-
-    const sumar = (id) => {
-        const item = carrito.find((item) => item._id === id);
-        item.cantidad += 1;
-        item.valortotal = item.price * item.cantidad;
-        setCarrito(carrito);
-        actualizarCarritoEnLocalStorage(carrito);
-        obtenerCarrito();
-
-    };
-
-    const restar = (id) => {
-        const item = carrito.find((item) => item._id === id);
-        if (item.cantidad != 1) {
-            item.cantidad -= 1;
-        }
-        item.valortotal = item.price * item.cantidad;
-        setCarrito(carrito);
-        actualizarCarritoEnLocalStorage(carrito);
-        obtenerCarrito();
-    };
-
-    if (!carrito) {
-        return <div className="text-center mt-16">Cargando...</div>;
+  const eliminarDelCarrito = (id) => {
+    const index = carrito.findIndex((item) => item._id === id);
+    if (index !== -1) {
+      carrito.splice(index, 1);
+      actualizarCarritoEnLocalStorage(carrito);
+      obtenerCarrito();
     }
+  };
 
-    return (
-        <div className="App">
-            {carrito.map((producto, index) => (
-                <div
-                    key={index}
-                    className="flex justify-around mt-4 mb-4 ml-40 mr-40 border-solid border rounded-lg"
-                >
-                    <div className=" flex justify-around w-48 mt-5 mb-5 ml-5 mr-5">
-                        <img
-                            src={producto.image}
-                        />
-                    </div>
+  const actualizarCarritoEnLocalStorage = (carritoData) => {
+    localStorage.setItem("carrito", JSON.stringify(carritoData));
+  };
 
-                    <div className="">
-                        <strong><h3 className="text-2xl mt-10 mb-10">{producto.name}</h3></strong>
-                        <br />
-                        <strong><a className="text-2xl">{producto.valortotal}</a></strong>
-                    </div>
+  const obtenerCarrito = () => {
+    const carritoData = JSON.parse(localStorage.getItem("carrito")) || [];
+    setCarrito(carritoData);
+  };
 
-                    <button onClick={() => restar(producto._id)}
-                        className="bg-color-secondary text-color-primary mt-20 mb-20 rounded w-10 h-10"
-                        type="button">-</button>
-                    <a className="mt-20 mb-20">{producto.cantidad}</a>
-                    <button onClick={() => sumar(producto._id)}
-                        className="bg-color-secondary text-color-primary mt-20 mb-20 rounded w-10 h-10"
-                        type="button">+</button>
-                    <button
-                        onClick={() => eliminarDelCarrito(producto._id)}
-                        className="bg-color-button-delete text-color-primary mt-20 mb-20 rounded w-32 h-10"
-                        type="button"><p className="">Eliminar</p></button>
-                </div>
+  const sumarTotal = (productos) => {
+    let total = 0;
+    for (const producto of productos) {
+      total += producto.valortotal;
+    }
+    return total;
+  };
 
-            ))}
+  const sumar = (id) => {
+    const item = carrito.find((item) => item._id === id);
+    item.cantidad += 1;
+    item.valortotal = item.price * item.cantidad;
+    setCarrito(carrito);
+    actualizarCarritoEnLocalStorage(carrito);
+    obtenerCarrito();
+  };
 
-            <div className="flex justify-around ml-60 mr-60 border-solid border rounded-lg">
-                <strong><h3 className="text-2xl mt-10 mb-10">Valor Total:</h3></strong>
+  const restar = (id) => {
+    const item = carrito.find((item) => item._id === id);
+    if (item.cantidad != 1) {
+      item.cantidad -= 1;
+    }
+    item.valortotal = item.price * item.cantidad;
+    setCarrito(carrito);
+    actualizarCarritoEnLocalStorage(carrito);
+    obtenerCarrito();
+  };
 
-                <strong><h3 className="text-2xl mt-10 mb-10">{sumarTotal(carrito)}</h3></strong>
-            </div>
+  if (!carrito) {
+    return <div className="text-center mt-16">Cargando...</div>;
+  }
 
+  const limpiarCarrito = () => {
+    setCarrito([]);
+    localStorage.removeItem("carrito");
+  };
+
+  const handleRealizarCompra = async () => {
+    try {
+      const simplifiedProducts = carrito.map((producto) => ({
+        productId: producto._id,
+        quantity: producto.cantidad,
+      }));
+      const response = await axios.post("http://localhost:4002/api/realizar-compra", {
+        products: simplifiedProducts,
+      });
+      console.log(response.data.message);
+      alert(response.data.message);
+      if (response.data.message === "Compra realizada exitosamente") {
+        limpiarCarrito();
+      }
+    } catch (error) {
+      console.error("Error al realizar la compra", error.response.data.message);
+      alert(error.response.data.message);
+    }
+  };
+    
+  return (
+
+    <div className="App">
+
+      {carrito.map((producto, index) => (
+        <div
+          key={index}
+          className="flex justify-around mt-4 mb-4 ml-40 mr-40 border-solid border rounded-lg"
+        >
+          <div className=" flex justify-around w-48 mt-5 mb-5 ml-5 mr-5">
+            <img
+              src={producto.image}
+            />
+          </div>
+
+          <div className="">
+            <strong><h3 className="text-2xl mt-10 mb-10">{producto.name}</h3></strong>
+            <br />
+            <strong><a className="text-2xl">{producto.valortotal}</a></strong>
+          </div>
+
+          <button onClick={() => restar(producto._id)}
+            className="bg-color-secondary text-color-primary mt-20 mb-20 rounded w-10 h-10"
+            type="button">-</button>
+          <a className="mt-20 mb-20">{producto.cantidad}</a>
+          <button onClick={() => sumar(producto._id)}
+            className="bg-color-secondary text-color-primary mt-20 mb-20 rounded w-10 h-10"
+            type="button">+</button>
+          <button
+            onClick={() => eliminarDelCarrito(producto._id)}
+            className="bg-color-button-delete text-color-primary mt-20 mb-20 rounded w-32 h-10"
+            type="button"><p className="">Eliminar</p></button>
         </div>
-    );
+      ))}
+
+      <div className="flex justify-around ml-60 mr-60 border-solid border rounded-lg">
+        <strong><h3 className="text-2xl mt-10 mb-10">Valor Total:</h3></strong>
+        <strong><h3 className="text-2xl mt-10 mb-10">{sumarTotal(carrito)}</h3></strong>
+      </div>
+
+      {/* <button onClick={() => { console.log(carrito) }} >Carrito en consola</button> */}
+
+      <div className="flex justify-center">
+        <button
+          onClick={handleRealizarCompra}
+          className="bg-color-button-delete text-color-primary mt-20 mb-20 rounded w-32 h-10 mr-4"
+          type="button"
+        >
+          Realizar Compra
+        </button>
+
+        <button
+          onClick={() => limpiarCarrito()}
+          className="bg-color-button-delete text-color-primary mt-20 mb-20 rounded w-32 h-10"
+          type="button"
+        >
+          Limpiar Carrito
+        </button>
+      </div>
+
+    </div>
+
+  );
+
 };
 
 export default ShoppingCartPage;
--- products/src/routes/products.routes.js
+++ products/src/routes/products.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { authRequired } from '../middlewares/validateToken.js'
 // Colocar después el authRequired en las rutas no públicas
-import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from '../controllers/products.controller.js'
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct, realizarCompra } from '../controllers/products.controller.js'
 import { createComment, getCommentsByProduct } from '../controllers/coment.controller.js'
 import axios from 'axios'
 
@@ -15,19 +15,20 @@ router.post('/products', createProduct)
 router.put('/products/:id', updateProduct)
 router.delete('/products/:id', deleteProduct)
 
+router.post('/realizar-compra', realizarCompra);
+
 // Rutas para comentarios
 router.post('/products/:id/comments', createComment)
 router.get('/products/:id/comments', getCommentsByProduct)
 
 export async function getUserModel() {
-    try {
-        const response = await axios.get('http://localhost:4001/user-model');
-        console.log('User model:', response.data);
-    } catch (error) {
-        console.error('Error fetching user model:', error);
-    }
+  try {
+    const response = await axios.get('http://localhost:4001/user-model');
+    console.log('User model:', response.data);
+  } catch (error) {
+    console.error('Error fetching user model:', error);
+  }
 }
-
 
 getUserModel();
 
